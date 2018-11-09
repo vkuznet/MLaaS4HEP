95ab0d6c1637a9cac5f8631a4310c0e6349c8252
Valentin Kuznetsov <vkuznet@gmail.com>
2018-11-09 07:46:47 -0500

    Add examples; model.json and setup.sh

---
+++ src/python/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# setup uproot
+export PYTHONPATH=/Users/vk/CMS/RandD/usr/lib/python2.7/site-packages:$PYTHONPATH
+# setup pyxrootd
+export PYTHONPATH=$PYTHONPATH:/Users/vk/CMS/RandD/xrootd-4.7.0/build/build/lib.macosx-10.13-x86_64-2.7
