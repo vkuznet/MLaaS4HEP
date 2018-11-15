7c1e85eb25b20b57a0685035988e923ec272f345
Valentin Kuznetsov <vkuznet@gmail.com>
2018-11-15 16:53:58 -0500

    remove test code since now it is covered by external models (ex_keras.py and ex_pytorch.py)

--- src/python/models.py
+++ src/python/models.py
@@ -4,7 +4,13 @@
 """
 File       : models.py
 Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
-Description: this module defines user based ML models for workflow
+Description: this module defines handles based ML models for MLaaS4HEP workflow.
+
+User should provide a model implementation (so far in either in Keras or PyTorch),
+see ex_keras.py and ex_pytorch.py, respectively for examples.
+
+The Trainer class defines a thin wrapper around user model and provides uniformat
+APIs to fit the model and yield predictions.
 """
 
 # system modules
@@ -19,6 +25,10 @@ from reader import xfile
 from generator import DataGenerator
 
 class Trainer(object):
+    """
+    Trainer class defines a think wrapper around given user model.
+    It defines basic `fit` and `predict` APIs.
+    """
     def __init__(self, model, verbose=0):
         self.model = model
         self.verbose = verbose
@@ -30,7 +40,13 @@ class Trainer(object):
                 print(self.model)
 
     def fit(self, data, y_train, **kwds):
-        "Fit implementation of the trainer"
+        """
+        Fit API of the trainer.
+
+        :param data: the ROOT IO data in form of numpy array of data and mask vectors.
+        :param y_train: the true values vector for input data.
+        :param kwds: defines input set of parameters for end-user model.
+        """
         xdf, mask = data[0], data[1]
         # cast values in data vector according to the mask
         xdf[np.isnan(mask)] = 0
@@ -45,8 +61,8 @@ class Trainer(object):
             raise NotImplemented
 
     def predict(self):
-        "Predict function of the trainer"
-        pass
+        "Predict API of the trainer"
+        raise NotImplemented
 
 def load_model(mfile):
     """
@@ -111,111 +127,3 @@ def train_model(model, files, params=None, specs=None, fout=None):
         trainer.fit(data, y_train, **kwds)
     if fout:
         trainer.save(fout)
-
-#
-# test code can be used as an example how to create and run workflow
-#
-def test_Keras_model(input_shape):
-    "Simple Keras model for testing purposes"
-    from keras.models import Sequential
-    from keras.layers import Dense, Activation
-
-    model = Sequential([
-        Dense(32, input_shape=input_shape),
-        Activation('relu'),
-        Dense(2),
-        Activation('softmax'),
-    ])
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    return model
-
-def test_PyTorch_model(input_shape):
-    "Simple PyTorch model for testing purposes"
-    from jarray.pytorch import JaggedArrayLinear
-    import torch
-    model = torch.nn.Sequential(
-        JaggedArrayLinear(input_shape, 5),
-        torch.nn.ReLU(),
-        torch.nn.Linear(5, 1),
-    )
-    return model
-
-def test_Keras(files, params=None, specs=None):
-    """
-    Test function demonstrates workflow of setting up data generator and train the model
-    over given set of files
-    """
-    from keras.utils import to_categorical
-    if not params:
-        params = {}
-    if not specs:
-        specs = {}
-    xfiles = [xfile(f) for f in files]
-    gen = DataGenerator(xfiles, params, specs)
-    epochs = specs.get('epochs', 10)
-    batch_size = specs.get('batch_size', 50)
-    shuffle = specs.get('shuffle', True)
-    split = specs.get('split', 0.3)
-    trainer = False
-    for data in gen:
-        x_train = np.array(data[0])
-        if not trainer:
-            input_shape = (np.shape(x_train)[-1],) # read number of attributes we have
-            trainer = Trainer(test_model(input_shape), verbose=params.get('verbose', 0))
-        print("x_train {} chunk of {} shape".format(x_train, np.shape(x_train)))
-        if np.shape(x_train)[0] == 0:
-            print("received empty x_train chunk")
-            break
-        # create dummy vector for y's for our x_train
-        y_train = np.random.randint(2, size=np.shape(x_train)[0])
-        y_train = to_categorical(y_train) # convert labesl to categorical values
-        print("y_train {} chunk of {} shape".format(y_train, np.shape(y_train)))
-        kwds = {'epochs':epochs, 'batch_size': batch_size, 'shuffle': shuffle, 'validation_split': split}
-        trainer.fit(data, y_train, **kwds)
-
-def test_PyTorch(files, params=None, specs=None):
-    """
-    Test function demonstrates workflow of setting up data generator and train
-    PyTorch model over given set of files
-    """
-    from jarray.pytorch import JaggedArrayLinear
-    import torch
-    if not params:
-        params = {}
-    if not specs:
-        specs = {}
-    xfiles = [xfile(f) for f in files]
-    gen = DataGenerator(xfiles, params, specs)
-    epochs = specs.get('epochs', 10)
-    batch_size = specs.get('batch_size', 50)
-    shuffle = specs.get('shuffle', True)
-    split = specs.get('split', 0.3)
-    model = False
-    for (x_train, x_mask) in gen:
-        if not model:
-            input_shape = np.shape(x_train)[-1] # read number of attributes we have
-            print("### input data: {}".format(input_shape))
-            model = test_PyTorch_model(input_shape)
-            print(model)
-        print("x_train chunk of {} shape".format(np.shape(x_train)))
-        print("x_mask chunk of {} shape".format(np.shape(x_mask)))
-        if np.shape(x_train)[0] == 0:
-            print("received empty x_train chunk")
-            break
-        data = np.array([x_train, x_mask])
-        preds = model(data).data.numpy()
-        print("preds chunk of {} shape".format(np.shape(preds)))
-
-
-def test(mname):
-    input_shape = (10,)
-    model = test_Keras_model(input_shape)
-    print(type(model))
-    model = test_PyTorch_model(input_shape[0])
-    print(type(model))
-    model = load_model(mname)
-
-if __name__ == '__main__':
-    test(sys.argv[1])
