e177fed5dd496e1e411d174298b50fc92e05f966
Valentin Kuznetsov <vkuznet@gmail.com>
2018-11-15 15:08:21 -0500

    Changes to setup local libs

--- src/python/setup.sh
+++ src/python/setup.sh
@@ -3,3 +3,7 @@
 export PYTHONPATH=/Users/vk/CMS/RandD/usr/lib/python2.7/site-packages:$PYTHONPATH
 # setup pyxrootd
 export PYTHONPATH=$PYTHONPATH:/Users/vk/CMS/RandD/xrootd-4.7.0/build/build/lib.macosx-10.13-x86_64-2.7
+if [ "`hostname`" == "vkair" ]; then
+    source /opt/anaconda2/etc/profile.d/conda.sh
+    export PYTHONPATH=$PYTHONPATH:/opt/anaconda2/lib/python2.7/site-packages
+fi
