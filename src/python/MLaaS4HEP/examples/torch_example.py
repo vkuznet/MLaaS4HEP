08e28168ca9d04d980718dedabdbe37d13978d69
Valentin <vkuznet@gmail.com>
2019-06-10 20:36:41 +0200

    Adjust code example

--- src/python/MLaaS4HEP/examples/torch_example.py
+++ src/python/MLaaS4HEP/examples/torch_example.py
@@ -1,13 +1,11 @@
-from reader import DataReader, xfile
-from tfaas import DataGenerator
-from jarray.pytorch import JaggedArrayLinear # implementation of LinearLayer for JaggedArray
+from MLaaS4HEP.reader import RootDataReader
+from MLaaS4HEP.jarray.pytorch import JaggedArrayLinear # implementation of LinearLayer for JaggedArray
 import torch
 
 params = {} # user provide model parameters, e.g. number of events to read,
 specs  = {} # data specs, e.g. jagged array dimentionality
 for fin in files:
-    fin = xfile(fin)                        # if we're given LFN create xrootd link
-    gen = DataGenerator(fin, params, specs) # read data chunk for given file, params/specs
+    gen = RootDataGenerator(fin, params, specs) # read data chunk for given file, params/specs
     model = False
     for (x_train, x_mask) in gen:
         if not model:
